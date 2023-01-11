45937975a60276ae4e8a10259ad125dd459d0b60
Thomas Morrell <tmorrell@users.noreply.github.com>
2023-01-10 16:50:06 -0800

    Modifications for InvenioRDM version of CaltechDATA

--- README.md
+++ README.md
@@ -10,8 +10,6 @@ Requires caltechdata_api (https://github.com/caltechlibrary/caltechdata_api)
 To use:
 - Copy submit_hg.py to your repository
 - Tag releases with versions in the form of "major.minor"
-     (New major releases will generate new CaltechDATA records, minor releases
-      will update CaltechDATA records)
 - Get a CaltechDATA access token: http://libanswers.caltech.edu/faq/211307
 - Fill out a metadata file.  An example metadata.json file is included as a
     starting point.  You can also grab metadata from CaltechDATA using the
@@ -21,4 +19,4 @@ To use:
 ### Note
 This script will make permanent records of your code in CaltechDATA.  These
 cannot be removed.  Take care to only run this script when you are ready to
-preserve your code.
+preserve your code, and all tags will be uploaded.
--- submit_hg.py
+++ submit_hg.py
@@ -1,112 +1,116 @@
-import os,json,datetime,argparse
+import os, json, datetime, argparse
 import hglib
 from caltechdata_api import caltechdata_edit
 from caltechdata_api import caltechdata_write
 
 # Submits a mercurial repository to CaltechDATA
-# To use copy this script and 
+# To use copy this script and type `python submit_hg.py metadata.json`
+# after filling out the metadata.json file with appropriate metadata
 
-# This script assumes that your hg repository uses tags like '2014.2'
-# where the major release identifier is separated from the minor release 
-# identifier by a period
+# The script will create CaltechDATA entries for every tag in your repository
 
 # You need to get a access token from CaltechDATA
-# http://libanswers.caltech.edu/faq/211307
+# http://libanswers.caltech.edu/faq/211307 and save it as the RDMTOK
+# environment variable
 
 # Requires python 3 and the python-hglib library
 # You can install python-hglib by typing pip install python-hglib
 # Requires caltechdata_api (https://github.com/caltechlibrary/caltechdata_api)
 
-def build_relation(client,version):
-    #Get url to specific tag from hg repo
-    full_url = client.paths()['default'.encode('utf-8')].decode('utf-8')
-    #Strip username
-    split_1 = full_url.split('@')
-    back = split_1[1]
-    final = 'https://'+back+'/commits/tag/'+version
-    return {'relatedIdentifier':final,\
-            'relatedIdentifierType':'URL',\
-            'relationType':'IsIdenticalTo'}
+
+def build_relation(client, version, metadata):
+    if client.paths() != {}:
+        # Get url to specific tag from hg repo
+        full_url = client.paths()["default".encode("utf-8")].decode("utf-8")
+        # Strip username
+        split_1 = full_url.split("@")
+        back = split_1[1]
+        final = "https://" + back + "/commits/tag/" + version
+        new = {
+            "relatedIdentifier": final,
+            "relatedIdentifierType": "URL",
+            "relationType": "IsIdenticalTo",
+        }
+        if "relatedIdentifiers" in metadata:
+            metadata["relatedIdentifiers"].append(new)
+        else:
+            metadata["relatedIdentifiers"] = [new]
+    return metadata
+
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description=\
-    "submit_hg submits a mercurial repository to caltechDATA (Invenio 3)")
-    parser.add_argument('json_file',nargs=1, help=\
-            'file name for json DataCite metadata file')
+    parser = argparse.ArgumentParser(
+        description="submit_hg submits a mercurial repository to caltechDATA (Invenio 3)"
+    )
+    parser.add_argument(
+        "json_file", nargs=1, help="file name for json DataCite metadata file"
+    )
 
     args = parser.parse_args()
 
-    token = os.environ['TINDTOK']
+    token = os.environ["RDMTOK"]
 
-    history_file = '.caltechdata_written'
+    history_file = ".caltechdata_written"
 
+    production = False
+    publish = True
 
     already_archived = []
     archived_ids = {}
-    major = set()
     if os.path.isfile(history_file):
-    #This file contains versions we've already submitted
-        infile = open(history_file,'r')
+        # This file contains versions we've already submitted
+        infile = open(history_file, "r")
         for j in infile:
             line = j.rstrip()
-            split = line.split(',')
+            split = line.split(",")
             archived_version = split[0]
-            ver_split = archived_version.split('.')
-            major.add(ver_split[0])
             already_archived.append(archived_version)
-            archived_ids[ver_split[0]]=split[1]
+            archived_ids[archived_version] = split[1]
 
-    repo = '.'
+    repo = "."
     client = hglib.open(repo)
     tags = client.tags()
-    for t in tags:  
-        version = t[0].decode('utf-8')
-        if version != 'tip':
-            if version not in already_archived:
-                hashv = t[2]
-                outfile = version+'.tgz'#open(version+'.tgz','w')
-                client.archive(outfile.encode('utf-8'),hashv)
-                #Determine is major or minor version
-                split = version.split('.')
-                mj = split[0]
-                if mj not in major:
-                    #New major release-New CaltechDATA record
-                    #Strip username
-                    metaf = open(args.json_file[0],'r')
-                    metadata = json.load(metaf)
-                    for d in metadata['dates']:
-                        if d['dateType'] == 'Updated':
-                            d['date'] = datetime.date.today().isoformat()
-                    metadata['version'] = version
-                    if 'relatedIdentifiers' in metadata:
-                        metadata['relatedIdentifiers'].append(build_relation(client,version))
-                    else:
-                        metadata['relatedIdentifiers']=[build_relation(client,version)]
-                    files = (outfile)
-                    response = caltechdata_write(metadata,token,files,True)
-                    print(response)
-                    new_id = response.split('/')[4].split('.')[0]
-                    outf = open(history_file,'a')
-                    outf.write(version+','+new_id)
-                    #Cleanup
-                    os.remove(outfile)
-                else:
-                    #Minor release - just edit the same CaltechDATA record
-                    metaf = open(args.json_file[0],'r')
-                    metadata = json.load(metaf)
-                    for d in metadata['dates']:
-                        if d['dateType'] == 'Updated':
-                            d['date'] = datetime.date.today().isoformat()
-                    metadata['version'] = version
-                    metadata['relatedIdentifiers'].append(build_relation(client,version))
-                    files = [outfile]
-                    response=caltechdata_edit(token,archived_ids[mj],metadata,files,['tgz'], True)
-                    print(response)
-                    #Not strictly necessary, but will prevent multiple edits
-                    new_id = response.split('/')[4].split('.')[0]
-                    outf = open(history_file,'a')
-                    outf.write(version+','+new_id)
-                    #Cleanup
-                    os.remove(outfile)
-
+    versions = []
+    for t in tags:
+        version = t[0].decode("utf-8")
+        if version != "tip":
+            versions.append(version)
+    versions.sort()
+    for version in versions:
+        if version not in already_archived:
+            hashv = t[2]
+            outfile = version + ".tgz"  # open(version+'.tgz','w')
+            client.archive(outfile.encode("utf-8"), hashv)
+            split = version.split(".")
+            mj = split[0]
+            metaf = open(args.json_file[0], "r")
+            metadata = json.load(metaf)
+            metadata["version"] = version
+            metadata = build_relation(client, version, metadata)
+            files = outfile
+            if already_archived == []:
+                print("Creating New Record")
+                # We need a New CaltechDATA record
+                response = caltechdata_write(
+                    metadata, token, files, production, publish=publish
+                )
+                new_id = response
+                already_archived.append(version)
+                archived_ids[version] = new_id
+            else:
+                # Otherwise create a new version of the existing CaltechDATA record
+                print("Creating New Version")
+                response = caltechdata_edit(
+                    archived_ids[versions[0]],
+                    metadata,
+                    token,
+                    files,
+                    production,
+                    publish=publish,
+                )
+                new_id = response.split("/")[1]
+            outf = open(history_file, "a")
+            outf.write(version + "," + new_id + "\n")
+            # Cleanup
+            os.remove(outfile)
